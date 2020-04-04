e9581e6dba1029cdd7cdc4d1b2fdbb7db8a39183
Mattias Stjernstedt <mstjernstedt@gmail.com>
2020-04-04 21:03:38 +0900

    Initial commit

---
+++ Assets/Scripts/ResourceProducer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceProducer : MonoBehaviour
+{
+	GameObject core;
+	public ResourceTypes type;
+
+	float timePassed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		core = FindObjectOfType<Core>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		timePassed += Time.deltaTime;
+		if(timePassed > 1)
+		{
+			ProduceResource();
+			timePassed = 0;
+		}
+    }
+
+	void ProduceResource()
+	{
+		core.GetComponent<Resources>().AddResource(type, 1);
+	}
+}
---
+++ Assets/Scripts/Resources.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ResourceTypes
+{
+	food,
+	fertilizer
+}
+
+public class Resources : MonoBehaviour
+{
+
+	public int food;
+	public int fertilizer;
+
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void AddResource(ResourceTypes type, int amount)
+	{
+		switch (type)
+		{
+			case ResourceTypes.food:
+				food += amount;
+				break;
+			case ResourceTypes.fertilizer:
+				break;
+			default:
+				break;
+		}
+	}
+}
