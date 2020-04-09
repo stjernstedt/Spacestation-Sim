67932012d25df20c63b318e5c73226ab1f8ee6e7
Mattias Stjernstedt <mstjernstedt@gmail.com>
2020-04-09 21:50:49 +0900

    added resource producers and costs

--- Assets/Scripts/Agricenter.cs
+++ Assets/Scripts/Agricenter.cs
@@ -2,17 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ResourceConsumer : MonoBehaviour
+public class Agricenter : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+		ResourceProducer producer = GetComponent<ResourceProducer>();
+		producer.AddCost(ResourceTypes.fertilizer, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+		
     }
 }
--- Assets/Scripts/ResourceProducer.cs
+++ Assets/Scripts/ResourceProducer.cs
@@ -7,27 +7,37 @@ public class ResourceProducer : MonoBehaviour
 	Resources resources;
 	public ResourceTypes type;
 
+	Dictionary<ResourceTypes, int> resourceCosts = new Dictionary<ResourceTypes, int>();
 	float timePassed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+	// Start is called before the first frame update
+	void Start()
+	{
 		resources = GetComponentInParent<Resources>();
-    }
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
 		timePassed += Time.deltaTime;
-		if(timePassed > 1)
+		if (timePassed > 1)
 		{
 			ProduceResource();
 			timePassed = 0;
 		}
-    }
+	}
 
-	void ProduceResource()
+	 void ProduceResource()
 	{
 		resources.AddResource(type, 1);
+		foreach (var item in resourceCosts)
+		{
+			resources.AddResource(item.Key, -item.Value);
+		}
 	}
+	public void AddCost(ResourceTypes type, int amount)
+	{
+		resourceCosts.Add(type, amount);
+	}
+
 }
